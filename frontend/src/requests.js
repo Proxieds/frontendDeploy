10f4181b5c29705ed8b16fe20cc5b2fa1f50567e
Victor Lieu <victor1@ualberta.ca>
2022-03-19 17:08:42 -0600

    Adding backend url

--- frontend/src/requests.js
+++ frontend/src/requests.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { BACKEND_PORT, BACKEND_URL } from "./constants";
 
+const url = process.env("HOST_URL") ? process.env("PROD") : `http://${BACKEND_URL}:${BACKEND_PORT}/`;
+
 export default axios.create({
-  baseURL: `http://${BACKEND_URL}:${BACKEND_PORT}/`,
+  baseURL: url,
   headers: {
     Authorization: localStorage.getItem('token')
     ? 'JWT ' + localStorage.getItem('access_token') : null,
