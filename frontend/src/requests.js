cdd1906b2c56348e18df072532cf07464e0b04c6
Victor Lieu <victor1@ualberta.ca>
2022-03-19 18:25:05 -0600

    sds

--- frontend/src/requests.js
+++ frontend/src/requests.js
@@ -3,7 +3,8 @@ import { BACKEND_PORT, BACKEND_URL } from "./constants";
 
 const url = process.env.REACT_APP_HOST_URL ? process.env.REACT_APP_HOST_URL : `http://${BACKEND_URL}:${BACKEND_PORT}/`;
 // const url = "https://deployservertest.herokuapp.com/";
-console.log(process.env.REACT_APP_HOST_URL)
+console.log(process.env.REACT_APP_HOST_URL);
+console.log(process.env.React_App_Host_Url);
 export default axios.create({
   baseURL: url,
   headers: {
