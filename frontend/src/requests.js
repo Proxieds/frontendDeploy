2eee229a2596e2039ec71d74f48568ce93f38fef
Victor Lieu <victor1@ualberta.ca>
2022-03-19 18:34:58 -0600

    s

--- frontend/src/requests.js
+++ frontend/src/requests.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { BACKEND_PORT, BACKEND_URL } from "./constants";
 
-const url = process.env.REACT_APP_HOST_URL ? process.env.REACT_APP_HOST_URL : `http://${BACKEND_URL}:${BACKEND_PORT}/`;
+const url = process.env.REACT_APP_HOST_URL2 ? process.env.REACT_APP_HOST_URL : `http://${BACKEND_URL}:${BACKEND_PORT}/`;
 // const url = "https://deployservertest.herokuapp.com/";
 console.log(process.env.REACT_APP_HOST_URL);
 console.log(process.env.React_App_Host_Url);
